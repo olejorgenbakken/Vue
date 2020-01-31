5f2c726d5ccd362f6995e49419d39550193d4308
Ole Jørgen Bakken <olejorgenbakken@gmail.com>
2020-01-31 10:31:34 +0100

    added context to links in footer

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -4,10 +4,10 @@
       <h3 class="navigation-header">Project overview</h3>
       <nav>
         <h4 class="navigation-header">Lectures</h4>
-        <router-link to="/lecture1">Lecture 1</router-link>
-        <router-link to="/lecture2">Lecture 2</router-link>
-        <router-link to="/lecture3">Lecture 3</router-link>
-        <router-link to="/lecture4">Lecture 4</router-link>
+        <router-link to="/lecture1">Lecture 1: Basics</router-link>
+        <router-link to="/lecture2">Lecture 2: Components and props</router-link>
+        <router-link to="/lecture3">Lecture 3: Components and props (cont.)</router-link>
+        <router-link to="/lecture4">Lecture 4: Routing</router-link>
       </nav>
 
       <nav id="links">
